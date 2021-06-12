bc7371985343328f8ccb098956ccd8ac12aaf353
David Zombar <davidzombar@gmail.com>
2021-06-12 11:59:39 -0500

    Assignment in progress

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const gifts = ["teddy bear", "drone", "doll"];
+//const gifts = ["teddy bear", "drone", "doll"];
+//
+//function wrapGifts(gifts) {
+//    for (let i = 0; i < gifts.length; i++) {
+//        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//    }
+//    return gifts;
+//}
+//
+//wrapGifts(gifts);
+const names = ["Lisa", "Kaitlyn", "Jan"]
+const event = ["Surprise", "Surprise2", "Surprise3"]
 
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped $(gifts[i])} and added a bow!`);
+function writeCards(names, event) {
+    for (let i =0; i< names.length; i++) {
+        newArray = []
+        newArray.push = (`Thank you, ${names[i]}, for the wonderful ${event} gift`)
+        debugger
     }
-    return gifts;
+    return newArray
 }
-
-wrapGifts(gifts);
