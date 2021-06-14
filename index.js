9439324b672a55ad201564ff3c0534b526214639
David Zombar <davidzombar@gmail.com>
2021-06-14 11:53:59 -0400

    Work in progress v1.2

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ const event = ["Surprise", "Surprise2", "Surprise3"]
 
 function writeCards(names, event) {
     for (let i =0; i< names.length; i++) {
-        newArray = []
+        let newArray = []
         newArray.push = (`Thank you, ${names[i]}, for the wonderful ${event} gift`)
-        debugger
     }
     return newArray
 }
